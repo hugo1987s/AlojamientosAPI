43cde30e4e6746e9e55b95f90276e7effc11d0dd
hugo1987s <hugo1987s@gmail.com>
2022-03-06 15:32:44 -0300

    Initial commit

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+    }
+}
